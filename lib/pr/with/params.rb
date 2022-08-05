49492b90d8595314595c7d51c6dd92b9d5ab52f0
2k-joker <khalil.kum@dailypay.com>
2022-08-05 18:32:54 -0400

    initial commit

---
+++ lib/pr/with/params.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "params/version"
+
+module Pr
+  module With
+    module Params
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
