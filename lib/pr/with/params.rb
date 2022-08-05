c184575155dd305e17a89229915431c54ee21753
2k-joker <khalil.kum@dailypay.com>
2022-08-05 19:51:03 -0400

    Update script to print informative messages

--- lib/pr/with/params.rb
+++ lib/pr/with/params.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 require_relative "params/version"
+require 'uri'
 
 module Pr
   module With
     module Params
       class Error < StandardError; end
-      # Your code goes here...
+      
+      def self.build_url_string(host:, path:, query:)
+        uri_query = URI.encode_www_form(query)
+        uri = URI::HTTPS.build(host: host, path: path, query: uri_query)
+        uri.to_s.gsub('&', '^&')
+      end
     end
   end
 end
