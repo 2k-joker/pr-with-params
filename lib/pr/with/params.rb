f950d2fa17f93da27b304b6401862073e3ddd88e
2k-joker <khalil.kum@dailypay.com>
2022-08-07 23:50:30 -0400

    Add support for setting params in yaml file

--- lib/pr/with/params.rb
+++ lib/pr/with/params.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "params/version"
+require_relative 'params/version'
+require_relative 'params/config_parser'
 require 'uri'
+require 'json'
 require 'launchy'
 
 module PR
@@ -9,10 +11,26 @@ module PR
     module Params
       class Error < StandardError; end
 
-      def self.open(host:, path:, query:)
+      extend self
+
+      def open(host:, path:, query:)
         uri_query = URI.encode_www_form(query)
         url_string = URI::HTTPS.build(host: host, path: path, query: uri_query).to_s
         Launchy.open(url_string)
+      end
+
+      def parse_config(file_path, scope)
+        config_options = ConfigParser.new(config_file_path: file_path, scope: scope).parse!
+      rescue StandardError => e
+        error_message = {
+          message: "Error parsing config file. Using defaults",
+          reason: e.message,
+          backtrace: e.backtrace&.last(10)
+        }.to_json
+
+        STDERR.puts "\e[35mWARNING\e[0m: " + error_message
+        
+        {}
       end
     end
   end
---
+++ lib/pr/with/params/config_parser.rb
@@ -0,0 +1,53 @@
+require 'yaml'
+
+module PR
+  module With
+    module Params
+      class ConfigParser
+        # Attributes
+        attr_reader :config_file_path, :scope, :parsed_config, :filtered_config
+
+        # Constants
+        VALID_CONFIG_KEYS = %i[base_branch template title labels assignees].freeze
+
+        def initialize(config_file_path:, scope: nil)
+          @config_file_path = config_file_path
+          @scope = scope&.to_sym || :default
+        end
+
+        def parse!
+          validate_file_type!
+          parse_yaml_config
+        end
+
+        private
+
+        def parse_yaml_config
+          @parsed_config = YAML::load(IO.read(config_file_path)).transform_keys(&:to_sym)
+          @filtered_config = scoped_config.transform_keys(&:to_sym).slice(*VALID_CONFIG_KEYS)
+        end
+
+        def scoped_config
+          if scope == :default || parsed_config.fetch(scope, {}).empty?
+            parsed_config[scope]
+          else
+            parsed_config[:default].merge(parsed_config[scope])
+          end
+        end
+
+        def validate_file_type!
+          raise ArgumentError.new('Config file path is invalid or file does not exist.') unless file_exists?
+          raise TypeError.new('Config file type must be YAML (.yaml or .yml)') unless yaml_file?
+        end
+
+        def yaml_file?
+          config_file_path.end_with?('.yaml') || config_file_path.end_with?('yml')
+        end
+
+        def file_exists?
+          File.file?(config_file_path)
+        end
+      end
+    end
+  end
+end
